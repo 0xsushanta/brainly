9426ddc0e28641523b69a799364aa8a424c1c601
sushantaruidas <sushantaruidas95@gmail.com>
2025-08-28 19:38:27 +0530

    fix typos

--- server/src/controllers/user.controllers.ts
+++ server/src/controllers/user.controllers.ts
@@ -4,14 +4,17 @@ import { UserModel } from '../models/user.model'
 export const signup = async(req:Request, res:Response):Promise<void>=>{
      const username= req.body.username
     const password=req.body.password
-    const existingUser= await UserModel.findOne(username,password)
+    const existingUser= await UserModel.findOne({
+        username,
+        password
+    })
     if(existingUser){
         res.status(400).json({msg:"user already exists"})
         return;
     }
    try {
      await UserModel.create({
-        userame:username,
+        username:username,
         password:password
     })
     res.status(201).json({msg: "user created"})
