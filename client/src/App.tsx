43e9d2769a4d8d4e2a30ad56b6dc878d38d0228b
sushantaruidas <sushantaruidas95@gmail.com>
2025-09-17 23:07:39 +0530

    feat: add CreateContentModal component with input fields and submit button

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { Button } from "./components/Button"
 import { Card } from "./components/Card"
+import { CreateContentModal } from "./components/CreateContentModal"
 import { Add } from "./icon/Add"
 import { Share } from "./icon/Share"
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
     <div className="p-4">
+      <CreateContentModal open={true} />
     <div className="flex justify-end gap-4">
       
       <Button variant="primary" text="Add Content" startIcon={<Add />} ></Button>
--- client/src/components/CreateContentModal.tsx
+++ client/src/components/CreateContentModal.tsx
@@ -1,3 +1,29 @@
-export function CreateContentModal(){
-    
+import { CrossIcon } from "../icon/CrossIcon";
+import { Button } from "./Button";
+
+
+export function CreateContentModal({ open, onClose }) {
+    return <div >
+        {open && <div className="h-full w-full bg-slate-500 fixed top-0 left-0 opacity-60 flex justify-center">
+            <div className="flex flex-col justify-center"> 
+            <span className="bg-white opacity-100 p-4 rounded">
+            <div className="flex justify-end">
+                <CrossIcon /> 
+            </div>
+            <div>
+            <Input placeholder="title" />
+            <Input placeholder="link"/>
+            </div>
+            <div className="flex justify-center">
+            <Button text="submit" variant="primary" />
+            </div>
+            </span>
+            </div>
+        </div>}
+    </div>
+}
+function Input ({onChange, placeholder}:{onChange:()=>void}){
+    return <div>
+        <input  placeholder={placeholder}  type={"text"} className="px-4 py-2 border rounded m-2" onChange={onChange}></input>
+    </div>
 }
