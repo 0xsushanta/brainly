d457d4f17703ce9fc1ae6481e856ee666ebdec07
sushantaruidas <sushantaruidas95@gmail.com>
2025-09-18 00:50:01 +0530

    feat: enhance CreateContentModal with close functionality and improve Button component to accept onClick prop

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { Button } from "./components/Button"
 import { Card } from "./components/Card"
 import { CreateContentModal } from "./components/CreateContentModal"
@@ -8,22 +9,26 @@ import { Share } from "./icon/Share"
 
 
 function App() {
-  
 
+const [modalOpen, setModelOpen]= useState(true)
   return (
     <>
-    <div className="p-4">
-      <CreateContentModal open={true} />
-    <div className="flex justify-end gap-4">
-      
-      <Button variant="primary" text="Add Content" startIcon={<Add />} ></Button>
-      <Button variant="secondary" text="Share Brain" startIcon={<Share />}></Button>
-      </div>
-      <div className="flex gap-4">
-        <Card type="x" link="https://twitter.com/Hiteshdotcom/status/1968201148365672568" title="hitesh post"/>
-        {/* <Card type="youtube" /> */}
-        <Card link="https://www.youtube.com/watch?v=leWuDQoDaYY" title="first video" type="youtube" />
-      </div>
+      <div className="p-4">
+        <CreateContentModal open={modalOpen} onClose={()=>{
+          setModelOpen(false)
+        }}/>
+        <div className="flex justify-end gap-4">
+
+          <Button onClick={()=>{
+            setModelOpen(true)
+          }} variant="primary" text="Add Content" startIcon={<Add />} ></Button>
+          <Button variant="secondary" text="Share Brain" startIcon={<Share />}></Button>
+        </div>
+        <div className="flex gap-4">
+          <Card type="x" link="https://twitter.com/Hiteshdotcom/status/1968201148365672568" title="hitesh post" />
+          {/* <Card type="youtube" /> */}
+          <Card link="https://www.youtube.com/watch?v=leWuDQoDaYY" title="first video" type="youtube" />
+        </div>
       </div>
     </>
   )
--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -4,7 +4,7 @@ interface ButtonProps {
     variant: "primary"| "secondary"
     text: string
     startIcon: ReactElement
-    
+    onClick?: ()=>void
      
     
 }
@@ -13,8 +13,8 @@ const variantClasses={
     "secondary":"bg-purple-200 text-purple-500"
 }
 const defaultStyle = "px-4 py-2 rounded-md font-light flex items-center"
-export function Button({variant,text,startIcon}:ButtonProps){
-    return <button className={`${variantClasses[variant]} ${defaultStyle}`}>
+export function Button({variant,text,startIcon, onClick}:ButtonProps){
+    return <button onClick={onClick} className={`${variantClasses[variant]} ${defaultStyle}`}>
         <div className="pr-2">
         {startIcon}
         </div>
